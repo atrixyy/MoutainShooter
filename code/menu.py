96b8fd774341f4d5ed19927d4ccba8ad3d2aa1e3
BeatrizC <bea.carmo27@gmail.com>
2026-07-23 21:13:35 -0300

    Adding classes generated from StaruML

---
+++ code/menu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Menu:
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+        self.run = None
+
+    def run(self, ):
+        pass
